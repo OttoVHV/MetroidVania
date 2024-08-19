8d6cdd0388cc5aad3129d4c67d4370f2b24370d7
ReiDosNoobs <leonardo48rodrigues@gmail.com>
2024-08-19 19:59:16 -0300

    Câmera
    câmera que segue o personagem

--- Assets/scripts/CameraFollow.cs
+++ Assets/scripts/CameraFollow.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    private Vector3 offset = new Vector3(0f, 0f, -10f);
+    private float smoothTime = 0.25f;
+    private Vector3 velocity = Vector3.zero;
+
+    [SerializeField] private Transform target;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,8 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 targetPosition = target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+
     }
 }
